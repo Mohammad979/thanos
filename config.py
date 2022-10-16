0c20195f320f8659946fcd568af1ee77c29c5b6e
ʑɑʑɓɦɑɪ <87512658+Zazbhai@users.noreply.github.com>
2022-10-16 21:14:17 +0530

    Update and rename Iconfig.py to config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+from sample_config import Config
+
+
+class Development(Config):
+    # get this values from the my.telegram.org
+    APP_ID = 21083129
+    API_HASH = "46f6c82fad537a7e306a2f1084dc0a62"
+    # the name to display in your alive message
+    ALIVE_NAME = "Շђคภ๏ร"
+    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+    DB_URI = "postgres://lvxjlgee:8FLuaV-Mw9KNFvkZ3nCHEOgjwa8jnsQG@jelani.db.elephantsql.com/lvxjlgee" # IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #DB_URI 
+    # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
+    STRING_SESSION = "1AZWarzUBu6Nd0k83n-URo3zZ-1Y0O-_LyT72N3l0-AAQdoQlhFX1CxHG6SMHDk56-RZVaMJYG-upvVq70uVOEhtBf0N0sByINBgqJkiyOJucjqVn7zWS7r8dgKdRFKAxtNErpfftAmRdrTtUx8FQvd5ry6EjoU0e1J6-yzD1hJMgPT8MP0Ne87m0PJwS8-UL3KzGK3fGF-05ZJ2Apcj_3-rz84a1RaZmtzHoHefrkUw1OvpjGjCA1EteJ7YBye0FjIgTdMgszxmlmG9yyUTkOjh-tkKI94ezjYmiiofRLzsj_NCJI0j0Z6JwQrDZ4-fRGcKdwCmxjrPWbVDnO_S0eYbB4eGpXIw="# IF U DONT KNOW HOW TO MAKE THEN GO IN THANOS SUPPORT AND TYPE #session 
+    # --------------------------------------------------
+
+    TG_BOT_TOKEN = "5799736509:AAEeurHHzYMOkVkfIZCXt_4ZCD9QauIBZHo"# create a new bot in @botfather and fill the following vales with bot token
+    # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
+    PRIVATE_GROUP_BOT_API_ID = -1001638681164
+    # command handler
+    COMMAND_HAND_LER = "."
+   # VCMODE = "True"
+    #VC_SESSION = "your assistant id session"
+    # command hanler for sudo
+    SUDO_COMMAND_HAND_LER = "."
+    # External plugins repo
+    EXTERNAL_REPO = "https://github.com/rishabhanand2/tha_plugins"
+    #don't edit this 
+    THANOSABUSE = "False"#don't edit this
